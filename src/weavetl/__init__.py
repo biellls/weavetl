bb803aa594c80da83dfbc3047bbb8c96036af713
biellls <biellls@users.noreply.github.com>
2025-10-23 17:55:56 +0200

    Refactor WeavetlProject to use Pydantic

--- src/weavetl/__init__.py
+++ src/weavetl/__init__.py
@@ -1,2 +1,19 @@
-def main() -> None:
-    print("Hello from weavetl!")
+"""WeaveTL package exports."""
+
+from .project import (
+    PROJECT_FILENAME,
+    WeavetlProject,
+    find_project_file,
+    init_project,
+    load_project,
+    project_file_path,
+)
+
+__all__ = [
+    "PROJECT_FILENAME",
+    "WeavetlProject",
+    "find_project_file",
+    "init_project",
+    "load_project",
+    "project_file_path",
+]
